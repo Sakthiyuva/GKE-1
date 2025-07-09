378f4c934ad8b9a072a0777061c79d14c3fcacfa
yuvasakthi S <155764035+Sakthiyuva@users.noreply.github.com>
2025-07-09 13:22:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from Cloud, happy clouding", 200
+    return "Hello from Cloud, be happy", 200
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
